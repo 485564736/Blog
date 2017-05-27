526e60c18c71edf796a95f84c68bf608dd45c132
485564736 <485564736@qq.com>
2017-05-27 18:19:36 +0800

    hh
    hh

--- config/web.php
+++
@@ -1,9 +0,0 @@
-<?php return array (
-  'web_title' => 'Blog',
-  'web_count' => '百度统计',
-  'web_status' => '0',
-  'seo_title' => '',
-  'keywords' => '',
-  'description' => '',
-  'copyright' => '',
-);
